fe59d22fc12d47999ba88543c46e446fad4ceaad
Luke <lukeabennetz@gmail.com>
2019-01-27 15:00:31 +0000

    movement and scalability changes

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 $(document).on('click', '.preview', function (e) {
     let $target = $(e.target); 
     let src = $target.prop('src');
-    let msg = $target.parent().find('.notes').text();
-    open_modal(src, msg);
+    open_modal(src);
 });
 
-function open_modal(src, msg) {
+function open_modal(src) {
     swal({
         imageUrl: src,
-        customClass: 'col-sm-12 col-md-11 col-xl-10',
+        customClass: 'zoomed',
         padding: 0,
         showConfirmButton: false,
         showCloseButton: true,
-        html: `<p class="notes">${msg}</p>`,
-        imageAlt: msg,
     });
 }
-
